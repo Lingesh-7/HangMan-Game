459b549c3e256fd97612fd5338bcb2deefa788dd
Lingesh-7 <lingesh.r.official@gmail.com>
2024-11-02 11:58:43 +0530

    Crated Project

---
+++ pr-7.py
@@ -0,0 +1,116 @@
+# import random as r
+# lists=['ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar', 'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat', 'goose', 'hawk', 'lion', 'lizard', 'llama', 'mole', 'monkey', 'moose', 'mouse', 'mule', 'newt', 'otter', 'owl', 'panda', 'parrot', 'pigeon', 'python', 'rabbit', 'ram', 'rat', 'raven', 'rhino', 'salmon', 'seal', 'shark', 'sheep', 'skunk', 'sloth', 'snake', 'spider', 'stork', 'swan', 'tiger', 'toad', 'trout', 'turkey', 'turtle', 'weasel', 'whale', 'wolf', 'wombat', 'zebra']
+# ran=r.choice(lists)
+# j=0
+
+# live=0
+# print("_"*len(ran))
+# print(ran)
+# ans=[]
+# g=input("Guessed letter:")
+# def my():
+#     for i in range(len(ran)):
+#         g=input("Guessed letter:")
+#         if g!=ran[i]:
+#             ans.append('_')
+            
+#         elif g==ran[i]:
+#             ans.append(ga
+# s
+# s
+#)
+#     print(ans)
+# print(ans)
+# my()
+# for i in ans:
+#     if ans=='_':
+#         my()
+# print(ans)
+
+import random as r
+#from replit import clear
+print("Welcome to Hangman Game!!!")
+print(""" 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    """)
+f=['ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar', 'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat', 'goose', 'hawk', 'lion', 'lizard', 'llama', 'mole', 'monkey', 'moose', 'mouse', 'mule', 'newt', 'otter', 'owl', 'panda', 'parrot', 'pigeon', 'python', 'rabbit', 'ram', 'rat', 'raven', 'rhino', 'salmon', 'seal', 'shark', 'sheep', 'skunk', 'sloth', 'snake', 'spider', 'stork', 'swan', 'tiger', 'toad', 'trout', 'turkey', 'turtle', 'weasel', 'whale', 'wolf', 'wombat', 'zebra']
+ran=r.choice(f).lower()
+ans=[]
+print(ran)
+anss=""
+stages= ['''
+     +---+
+         |
+         |
+         |
+        ===''', '''
+    +---+
+    O   |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+    |   |
+        |
+       ===''', '''
+   +---+
+   O   |
+  /|   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  /    |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  / \  |
+       ===''']
+print(stages[0])
+live=0
+
+for j in range(len(ran)):
+    ans.append('_')
+    anss+=ans[j]
+print(anss)
+
+enf_of_game=False
+
+while enf_of_game!=True:
+    g=input("Guess:").lower()
+    #clear()
+    if g in ans:
+        print(f"You Have Already guessed {g}")
+    for i in range(len(ran)):
+        if g==ran[i]:
+            ans[i]=g
+
+    if g not in ran:
+        print(f"{g} is not in the word")
+        live+=1
+        print(stages[live])
+        print(f"\nRemaining Lives:{6-live}\n")
+        if live==6:
+            enf_of_game=True
+            print(f"The Word is {ran.upper()}\n")
+            print("You lose!!!")       
+    print(ans)
+
+    if '_' not in ans:
+        enf_of_game=True
+        print("\nYou win!!!")
+        
